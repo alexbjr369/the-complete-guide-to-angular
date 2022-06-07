b701c3729388c19ac315954a9b7e45a8f70ef976
alexbjr369 <alexbjr369@gmail.com>
2022-06-07 20:22:29 -0300

    commit

--- 02-the-basics/src/app/app.module.ts
+++ 02-the-basics/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ServerComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
