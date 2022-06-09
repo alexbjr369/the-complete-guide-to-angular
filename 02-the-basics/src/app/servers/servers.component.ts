96170f4ba06fcc5ced0daa84856af67abcf62c1a
alexbjr369 <alexbjr369@gmail.com>
2022-06-09 11:20:01 -0300

    commit

--- 02-the-basics/src/app/servers/servers.component.ts
+++ 02-the-basics/src/app/servers/servers.component.ts
@@ -10,6 +10,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server was created!';
   serverName = '';
   serverCreated = false;
+  servers = ['Test server', 'Test server 2'];
 
   constructor() {
     setTimeout(() => {
@@ -21,6 +22,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus =
       'Server was created! name is ' + this.serverName;
   }
