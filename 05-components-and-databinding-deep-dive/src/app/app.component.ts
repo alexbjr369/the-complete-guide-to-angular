93ccd704a9a7538974b76ec5d78e0c0788310cee
alexbjr369 <alexbjr369@gmail.com>
2022-06-15 01:01:09 -0300

    commit

--- 05-components-and-databinding-deep-dive/src/app/app.component.ts
+++ 05-components-and-databinding-deep-dive/src/app/app.component.ts
@@ -9,4 +9,23 @@ export class AppComponent {
   serverElements: any = [
     { type: 'server', name: 'Test server', content: 'just a test!' },
   ];
+
+  onServerAdded(serverData: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent,
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {
+    serverName: string;
+    serverContent: string;
+  }) {
+    this.serverElements.push({
+      type: 'server',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent,
+    });
+  }
 }
