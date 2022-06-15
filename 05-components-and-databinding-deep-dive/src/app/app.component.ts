3c7a6f7dba618365e4bcbd5164e5fea8fbc0cd8f
alexbjr369 <alexbjr369@gmail.com>
2022-06-15 00:36:09 -0300

    commit

--- 05-components-and-databinding-deep-dive/src/app/app.component.ts
+++ 05-components-and-databinding-deep-dive/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  serverElements: any = [];
+  serverElements: any = [
+    { type: 'server', name: 'Test server', content: 'just a test!' },
+  ];
 }
