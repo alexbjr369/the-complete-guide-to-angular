fc69088ee07ed1abcaffbefef84e402d90a19ddc
alexbjr369 <alexbjr369@gmail.com>
2022-06-16 17:09:59 -0300

    commit

--- 09-using-services-and-dependency-injection/src/app/app.module.ts
+++ 09-using-services-and-dependency-injection/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccountComponent } from './account/account.component';
+import { NewAccountComponent } from './new-account/new-account.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, AccountComponent, NewAccountComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
