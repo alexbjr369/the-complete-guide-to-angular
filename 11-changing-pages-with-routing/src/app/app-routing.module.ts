e8c3c19573eb9e4f139b936dd522076d97b858b2
alexbjr369 <alexbjr369@gmail.com>
2022-06-17 17:20:51 -0300

    commit

--- 11-changing-pages-with-routing/src/app/app-routing.module.ts
+++ 11-changing-pages-with-routing/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ServersComponent } from './servers/servers.component';
+import { UserComponent } from './users/user/user.component';
 import { UsersComponent } from './users/users.component';
 
 const routes: Routes = [
@@ -16,6 +17,10 @@ const routes: Routes = [
   {
     path: 'users',
     component: UsersComponent,
+  },
+  {
+    path: 'users/:id',
+    component: UserComponent,
   },
 ];
 
