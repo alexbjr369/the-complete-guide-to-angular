e4ccf09ed570514e7a836d5cdf7e8366bc9461bf
alexbjr369 <alexbjr369@gmail.com>
2022-06-18 10:15:53 -0300

    commit

--- 11-changing-pages-with-routing/src/app/app-routing.module.ts
+++ 11-changing-pages-with-routing/src/app/app-routing.module.ts
@@ -15,22 +15,26 @@ const routes: Routes = [
   {
     path: 'servers',
     component: ServersComponent,
-  },
-  {
-    path: 'servers/:id',
-    component: ServerComponent,
-  },
-  {
-    path: 'servers/:id/edit',
-    component: EditServerComponent,
+    children: [
+      {
+        path: ':id',
+        component: ServerComponent,
+      },
+      {
+        path: ':id/edit',
+        component: EditServerComponent,
+      },
+    ],
   },
   {
     path: 'users',
     component: UsersComponent,
-  },
-  {
-    path: 'users/:id/:name',
-    component: UserComponent,
+    children: [
+      {
+        path: ':id/:name',
+        component: UserComponent,
+      },
+    ],
   },
 ];
 
